704dfeda0f0c79408e467dc6ce0bdc718055cad5
ihm-ts <ihm-ts@protonmail.com>
2021-03-23 14:58:26 +0100

    meta: test listing authors on pages

--- assets/js/contribute.js
+++ assets/js/contribute.js
@@ -8,7 +8,8 @@
             if(url.split(editable).length===1 && url.endsWith(editable)) {
                 continue;
             }
-            filename = `${editable}${url.split(editable)[1]}`
+            let index = url.indexOf(editable)+editable.length;
+            filename = `${editable}${url.substring(index)}`
             // Remove last slash
             filename = filename.substring(0,filename.length-1);
         }
@@ -21,5 +22,57 @@
     if(content===undefined) {
         return;
     }
-    content.innerHTML = `<a style="font-size: 0.8em;" href="https://github.com/tswow/tswow-wiki/edit/main/_${filename}.md">Edit page</a><p></p> ${content.innerHTML}`;
+
+    const api_path = `https://api.github.com/repos/tswow/tswow-wiki/commits?path=_${filename}.md`
+    const file_path = `https://github.com/repos/tswow/tswow-wiki/_${filename}.md`
+    const edit_path = `https://github.com/tswow/tswow-wiki/edit/main/_${filename}.md`
+
+    let jsonString = localStorage[api_path] === undefined ? '{"timestamp": 0}' : localStorage[api_path];
+    let curStorage = JSON.parse(jsonString);
+
+    function shouldQuery() {
+        try {
+            return  curStorage.data === undefined || (Date.now()-curStorage.timestamp > 86400);
+        } catch(err) {
+            return true;
+        }
+    }
+
+    function handleCommits(commits) {
+        let authors = {}
+        try {
+            for(const commit of commits) {
+                const url = commit.author.html_url;
+                const avatar = commit.author.avatar_url;
+                const id = commit.author.id;
+                authors[id] = {url, avatar}
+            }
+        } catch(err) {}
+        authors = Object.values(authors);
+        content.innerHTML = `<div style="font-family: consolas; font-size: 0.8em;"> 
+        ${authors.length===0?`<a href=${file_path}>View authors</a>`:
+        `Authors:${
+            authors.map(x=>`
+                <a href="${x.url}"> <img src="${x.avatar}" width="28" height="28" /> </a>
+            `).join(' ')}`}
+        </br>
+        <a href="${edit_path}">
+            Edit page
+        </a>
+        <p></p> 
+        </div>
+        ${content.innerHTML}`;
+    }
+
+    if(shouldQuery()) {
+        $.getJSON(api_path, (data)=>{
+            // if we display authors, they should always be correct.
+            handleCommits(data);
+            if(Array.isArray(data) && data.length > 0) {
+                localStorage[api_path] = JSON.stringify({data: data, timestamp: Date.now()});
+            }
+        });
+    } else {
+        handleCommits(curStorage.data);
+    }
 }());
